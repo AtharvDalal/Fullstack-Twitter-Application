9adfc86418d1a4306eb0ffef7fb9df93ad01eff5
Atharv Dalal <sangamdalal24@gmail.com>
2024-04-03 22:44:59 +0530

    PostgreSQL +Prisma ✅

---
+++ server/src/app/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express'
+import bodyParser from 'body-parser';
+import { prismaClient } from '../client/db';
+
+
+
+export async function initServer() {
+    const app = express()
+     
+  app.use(bodyParser.json())
+
+  prismaClient.user.create({
+    data:{
+        
+    }
+  })
+    const graphqlServer = new ApolloServer({
+        typeDefs:`
+             type Query {
+                sayHello:String
+             }
+
+        `,
+        resolvers:{
+            Query:{
+                sayHello:()=>`Hello From Graphql Server`
+            },
+        }
+    });
+
+    await graphqlServer.start();
+    app.use('/graphql',expressMiddleware(graphqlServer))
+
+    return app;
+}
+
